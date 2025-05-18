dc3aaaf437afabd8d13f1038e531e1d228fc2095
collins muriuki <muriukicollins48@gmail.com>
2025-05-18 20:52:58 +0300

    final commit

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python
-
-from user import User
-
+#!/usr/bin/env py
+from lib.user import User
 import random
 
 class Teacher(User):
-
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "Python basics",
+            "OOP concepts",
+            "Inheritance",
+            "Testing",
+            "Debugging"
+        ]
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
